d269b4dc9ec0a12749ab07114c5c114727576ff7
JonasHiltl <71456708+JonasHiltl@users.noreply.github.com>
2021-01-31 18:52:45 +0100

    Update networkUpWithCouchDB.sh

--- network/scripts/networkUpWithCouchDB.sh
+++ network/scripts/networkUpWithCouchDB.sh
@@ -2,6 +2,6 @@
 
 pushd ~/mount/network/docker
 
-docker-compose -f ./docker-compose-test-net.yaml -f ./docker-compose-couch.yaml up -d 
+docker-compose -f ./docker-compose-test-net.yaml -f ./docker-compose-couch.yaml -f ./docker-compose-ca.yaml up -d 
 
 popd
